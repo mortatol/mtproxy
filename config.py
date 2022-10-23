d6e8ed3f20fcd1fd7087948f77b8fc76d5d2c933
Parsa <42109239+Parsoolak@users.noreply.github.com>
2022-10-23 20:51:03 +0330

    Update config for test

--- config.py
+++ config.py
@@ -2,26 +2,14 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
-    # "tg2": "0123456789abcdef0123456789abcdef",
+    "tg":  "mo23456789abcdefla23456789abcdez"
 }
 
 MODES = {
-    # Classic mode, easy to detect
     "classic": False,
-
-    # Makes the proxy harder to detect
-    # Can be incompatible with very old clients
     "secure": False,
-
-    # Makes the proxy even more hard to detect
-    # Can be incompatible with old clients
     "tls": True
 }
 
-# The domain for TLS mode, bad clients are proxied there
-# Use random existing domain, proxy checks it on start
-# TLS_DOMAIN = "www.google.com"
-
-# Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+TLS_DOMAIN = "www.google.com"
+AD_TAG = "92628c2693651edd8be60993a91bb86d"
